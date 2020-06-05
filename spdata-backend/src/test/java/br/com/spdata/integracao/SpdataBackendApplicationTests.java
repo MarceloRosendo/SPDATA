224b05603365ff4cc29bb51143e68f75f65d3952
MarceloRosendo <marcelomax159@gmail.com>
2020-06-04 21:51:45 -0300

    first commit

---
+++ spdata-backend/src/test/java/br/com/spdata/integracao/SpdataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spdata.integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
