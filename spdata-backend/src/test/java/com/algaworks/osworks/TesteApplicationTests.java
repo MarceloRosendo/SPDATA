a07a5d7026cc0fe93eb99aca0519bda01389bba2
tiagomf29 <tiagomf29@hotmail.com>
2020-06-26 17:25:11 -0300

    Ajustes de estrutura do projeto

--- spdata-backend/src/test/java/com/algaworks/osworks/TesteApplicationTests.java
+++ spdata-backend/src/test/java/com/algaworks/osworks/TesteApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.spdata.integracao;
+package com.algaworks.osworks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpdataBackendApplicationTests {
+class TesteApplicationTests {
 
 	@Test
 	void contextLoads() {
