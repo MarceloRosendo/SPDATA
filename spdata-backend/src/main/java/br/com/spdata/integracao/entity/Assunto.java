2aaca7c84aa227ef9576ebe78d1c53807e6ea78c
josesmargds <josesmargds@gmail.com>
2020-06-29 21:25:47 -0300

    Criação da classe Assunto

---
+++ spdata-backend/src/main/java/br/com/spdata/integracao/entity/Assunto.java
@@ -0,0 +1,31 @@
+package br.com.spdata.integracao.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_assunto")
+public class Assunto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private int id;
+	private String descricao;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}	
+
+}
