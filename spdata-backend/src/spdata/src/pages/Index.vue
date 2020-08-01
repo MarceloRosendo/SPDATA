86bdbf4694738f55c8c683665359c8b25d853e0f
Marcelo <marcelomax159@gmail.com>
2020-07-31 21:27:03 -0300

    Melhorando organização do footer, adição da logo do facebook

--- spdata-backend/src/spdata/src/pages/Index.vue
+++ spdata-backend/src/spdata/src/pages/Index.vue
@@ -147,72 +147,94 @@
         </div>
         <div class="fourty-view-cards">
           <q-card class="my-card" flat bordered>
-            <q-parallax src="https://politica.estadao.com.br/blogs/crop/1200x900/fausto-macedo/wp-content/uploads/sites/41/2016/08/santa-casa-sao-sebastiao-paraiso.jpg?xcd_image_optimization=false" :height="500" />
+            <q-parallax
+              src="https://politica.estadao.com.br/blogs/crop/1200x900/fausto-macedo/wp-content/uploads/sites/41/2016/08/santa-casa-sao-sebastiao-paraiso.jpg?xcd_image_optimization=false"
+              :height="500"
+            />
 
             <q-card-section>
               <div class="text-overline text-orange-9">Santa Casa de Paraíso</div>
               <div class="text-h5 q-mt-sm q-mb-xs">Naiara Cristina Moraes</div>
-              <div class="text-caption text-grey">
-                É bastante interessante participar do curso de capacitação, pois para mim o sistema é novo, haja vista, que assumi recentemente o cargo de gestora de TI, com certeza é um grande investimento profissional.
-              </div>
+              <div
+                class="text-caption text-grey"
+              >É bastante interessante participar do curso de capacitação, pois para mim o sistema é novo, haja vista, que assumi recentemente o cargo de gestora de TI, com certeza é um grande investimento profissional.</div>
             </q-card-section>
           </q-card>
 
           <q-card class="my-card fourty-view-card-2" flat bordered>
-            <q-parallax src="https://1.bp.blogspot.com/-3wnH8g9Eqtk/V-pbKFL5fyI/AAAAAAAAKP4/ZmrWhSZ4sSA9L1cxcWtte2ZMUYU4vSJmACLcB/s1600/HNSD-fachada.jpg" :height="500" />
+            <q-parallax
+              src="https://1.bp.blogspot.com/-3wnH8g9Eqtk/V-pbKFL5fyI/AAAAAAAAKP4/ZmrWhSZ4sSA9L1cxcWtte2ZMUYU4vSJmACLcB/s1600/HNSD-fachada.jpg"
+              :height="500"
+            />
             <q-card-section>
               <div class="text-overline text-orange-9">Hospital Nossa Senhora das Dores</div>
               <div class="text-h5 q-mt-sm q-mb-xs">Welisson Geraldo dos Reis</div>
-              <div class="text-caption text-grey">
-                O Software de Gestão Hospitalar da SPDATA é um ferramenta indispensável para gerir/administrar uma instituição de saúde com foco voltado para os processos institucionais. Outro ponto forte da empresa é o SAC que é nota 10.
-              </div>
+              <div
+                class="text-caption text-grey"
+              >O Software de Gestão Hospitalar da SPDATA é um ferramenta indispensável para gerir/administrar uma instituição de saúde com foco voltado para os processos institucionais. Outro ponto forte da empresa é o SAC que é nota 10.</div>
             </q-card-section>
           </q-card>
         </div>
         <div class="fourty-view-cards">
           <q-card class="my-card" flat bordered>
-            <q-parallax src="https://s2.glbimg.com/ZcGJOKHqluKI-tEyUgHfvr4u4HU=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/S/u/AMwyB5SAiWXip9jBEOlw/hnsc.jpg" :height="500" />
+            <q-parallax
+              src="https://s2.glbimg.com/ZcGJOKHqluKI-tEyUgHfvr4u4HU=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/S/u/AMwyB5SAiWXip9jBEOlw/hnsc.jpg"
+              :height="500"
+            />
 
             <q-card-section>
-              <div class="text-overline text-orange-9">Hospital Nossa Sra da Conceição de Para de Minas</div>
+              <div
+                class="text-overline text-orange-9"
+              >Hospital Nossa Sra da Conceição de Para de Minas</div>
               <div class="text-h5 q-mt-sm q-mb-xs">Renato Melo</div>
-              <div class="text-caption text-grey">
-                Todo Gestor precisa de muitas informações no seu dia a dia. E o sistema SPDATA é referência como fonte de informações gerenciais para a tomada das decisões.
-              </div>
+              <div
+                class="text-caption text-grey"
+              >Todo Gestor precisa de muitas informações no seu dia a dia. E o sistema SPDATA é referência como fonte de informações gerenciais para a tomada das decisões.</div>
             </q-card-section>
           </q-card>
 
           <q-card class="my-card fourty-view-card-2" flat bordered>
-            <q-parallax src="https://extra.globo.com/incoming/11086522-a77-8ec/w976h550-PROP/hospital-nossa-senhora-das-dores.jpg" :height="500" />
+            <q-parallax
+              src="https://extra.globo.com/incoming/11086522-a77-8ec/w976h550-PROP/hospital-nossa-senhora-das-dores.jpg"
+              :height="500"
+            />
             <q-card-section>
               <div class="text-overline text-orange-9">Hospital Nossa Senhora das Dores</div>
               <div class="text-h5 q-mt-sm q-mb-xs">Welisson Geraldo dos Reis</div>
-              <div class="text-caption text-grey">
-                O Software de Gestão Hospitalar da SPDATA é um ferramenta indispensável para gerir/administrar uma instituição de saúde com foco voltado para os processos institucionais. Outro ponto forte da empresa é o SAC que é nota 10.
-              </div>
+              <div
+                class="text-caption text-grey"
+              >O Software de Gestão Hospitalar da SPDATA é um ferramenta indispensável para gerir/administrar uma instituição de saúde com foco voltado para os processos institucionais. Outro ponto forte da empresa é o SAC que é nota 10.</div>
             </q-card-section>
           </q-card>
         </div>
       </div>
-      <dir class="footer">
-
-      </dir>
+      <div class="footer">
+        <div class="footer-col-1">Redes sociais
+          <div class="footer-btn-container">
+            <q-btn href="https://www.facebook.com/Comunidade.SPDATA" color="white" class="footer-icon" icon="img:https://image.flaticon.com/icons/svg/59/59439.svg" round/>
+            <q-btn color="white" class="footer-icon" icon="img:https://image.flaticon.com/icons/svg/2111/2111532.svg" round/>
+            <q-btn color="white" class="footer-icon" icon="img:https://image.flaticon.com/icons/svg/733/733635.svg" round/>
+          </div>
+        </div>
+        <div class="footer-col-1">Informações</div>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
+
 window.onscroll = function() {
   scrollFunction();
 };
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").classList.add('header-colapsed')
+    document.getElementById("header").classList.add("header-colapsed");
     document.getElementById("links-header").style.display = "none";
   } else {
     document.getElementById("links-header").style.display = "flex";
-    document.getElementById("header").classList.remove('header-colapsed')
+    document.getElementById("header").classList.remove("header-colapsed");
   }
 }
 
@@ -262,7 +284,7 @@ export default {
       }
       return 9;
     },
-    contact(){
+    contact() {
       this.$router.push("/contato");
     }
   }
